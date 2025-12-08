6760a040c45832f71b65d36baa6d298b79529634
Kevin Armbruster <k.armbruster91@gmail.com>
2025-12-08 17:13:16 +0100

    Migrate to Eleventy with reusable components

---
+++ .eleventy.js
@@ -0,0 +1,20 @@
+module.exports = function(eleventyConfig) {
+  // Pass through static assets without processing
+  eleventyConfig.addPassthroughCopy({"images": "images"});
+  eleventyConfig.addPassthroughCopy({"resources": "resources"});
+  eleventyConfig.addPassthroughCopy("src/css");
+  eleventyConfig.addPassthroughCopy("src/js");
+  eleventyConfig.addPassthroughCopy({"CNAME": "CNAME"});
+
+  return {
+    dir: {
+      input: "src",
+      output: "dist",
+      includes: "_includes",
+      layouts: "_layouts"
+    },
+    templateFormats: ["html", "md", "njk"],
+    htmlTemplateEngine: "njk",
+    markdownTemplateEngine: "njk"
+  };
+};
